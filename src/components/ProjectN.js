db880c3608802048c8505f3369ac3f1693d59768
Abi James <a.james1@live.co.uk>
2020-02-07 12:12:48 +0000

    image loader

--- src/components/ProjectN.js
+++ src/components/ProjectN.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Zoom from 'react-reveal/Zoom'
 import Fade from 'react-reveal/Fade'
+import greenGarms from '../images/green-garms.png'
+import swell from '../images/swell.png'
+import makeItCount from '../images/make-it-count.png'
+import lifeOnMars from '../images/life-on-mars.png'
+import tetris from '../images/tetris.png'
 
 const ProjectN = () => {
 
@@ -13,31 +18,31 @@ const ProjectN = () => {
       <div className='project-box'>
         <Fade bottom>
           <div className='single-project-title'>Green Garms - A Sustainable Fashion App</div>
-          <img className='project-img' src='./images/green-garms.png' alt='Green Garms' />
+          <img className='project-img' src={greenGarms} alt='Green Garms' />
         </Fade>
       </div>
       <div className='project-box'>
         <Fade bottom>
-          <img className='project-img' src='./images/swell.png' alt='Swell' />
+          <img className='project-img' src={swell} alt='Swell' />
           <div className='single-project-title'>Swell - An App for Surfers</div>
         </Fade>
       </div>
       <div className='project-box'>
         <Fade bottom>
           <div className='single-project-title'>Make It Count - A Political App</div>
-          <img className='project-img' src='./images/make-it-count.png' alt='Make It Count' />
+          <img className='project-img' src={makeItCount} alt='Make It Count' />
         </Fade>
       </div>
       <div className='project-box'>
         <Fade bottom>
-          <img className='project-img' src='./images/life-on-mars.png' alt='Life On Mars' />
+          <img className='project-img' src={lifeOnMars} alt='Life On Mars' />
           <div className='single-project-title'>Life On Mars - A Mars Weather App</div>
         </Fade>
       </div>
       <div className='project-box'>
         <Fade bottom>
           <div className='single-project-title'>Tetris</div>
-          <img className='project-img' src='./images/tetris.png' alt='Tetris' />
+          <img className='project-img' src={tetris} alt='Tetris' />
         </Fade>
       </div>
     </div>
